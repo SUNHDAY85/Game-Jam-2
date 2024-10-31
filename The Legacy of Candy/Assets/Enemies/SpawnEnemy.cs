71050175fa7ce773a7ab00140dc74c5575428a8f
Wbeimar Agudelo <andrey.agudelo12@gmail.com>
2024-10-31 14:27:47 -0500

    EnemyMovement

---
+++ The Legacy of Candy/Assets/Enemies/SpawnEnemy.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+{
+    public List<GameObject> enemyList;
+    // Start is called before the first frame update
+    void Start()
+    {
+        EnemySpawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void EnemySpawn()
+    {
+        Instantiate(enemyList[SelectRandomEnemy()], transform.position, enemyList[SelectRandomEnemy()].transform.rotation);
+    }
+
+    private int SelectRandomEnemy() {
+        return Random.Range(0, enemyList.Count);
+    }
+}
