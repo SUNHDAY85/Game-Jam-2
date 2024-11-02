4d028f40038e472456415160ced1a40c26689667
Wbeimar Agudelo <andrey.agudelo12@gmail.com>
2024-11-02 11:29:33 -0500

    Enemy Movement

--- The Legacy of Candy/Assets/Scripts/Enemies/EnemyMovement.cs
+++ The Legacy of Candy/Assets/Scripts/Enemies/EnemyMovement.cs
@@ -7,45 +7,89 @@ public class EnemyMovement : MonoBehaviour
     //Declarations
     public float speedMovemen;
 
-    [SerializeField] private bool isJump;
-    [SerializeField] private bool isMovingLeft;
+    private bool isJump;
+    private bool isMovingLeft;
 
-    [SerializeField] private int changeDirection = 1;
+    private int changeDirection = 1;
+    private int action = 0;
 
     private Rigidbody2D enemyRigidbody2D;
-
     private SpriteRenderer enemySpriteRender;
 
-    [SerializeField] private AudioClip jumpAudioClip;
+    public AudioClip jumpAudioClip;
 
     // Start is called before the first frame update
     void Start()
     {
         enemyRigidbody2D = GetComponent<Rigidbody2D>();
         enemySpriteRender = GetComponent<SpriteRenderer>();
+
+        InvokeRepeating("ChangeAction",0,10);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Movement();
+        SelectAction();
+    }
+
+    private void ChangeAction()
+    {
+        if( Random.Range(0, 100) < 80)
+        {
+            action = 1;
+        }
+        else
+        {
+            action = 0;
+        }
+    }
+
+    private void SelectAction()
+    {
+        
+
+        switch (action)
+        {
+            default:
+                StayStill();
+                break;
+            case 1:
+                Movement();
+                break;
+        }
+    }
+
+    private void StayStill()
+    {
+
     }
 
     private void Movement()
     {
         if (!isJump)
         {
-            transform.Translate(Vector3.right * speedMovemen * Time.deltaTime);
+            transform.Translate(Vector3.right * speedMovemen * changeDirection * Time.deltaTime);
         }
     }
 
+    private void Jump()
+    {
+        isJump = true;
+        AudioManager.instance.PlaySFX(jumpAudioClip);
+
+        enemyRigidbody2D.AddForce(new Vector2(5 * changeDirection , 5), ForceMode2D.Impulse);
+    }
+
+    
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.collider.CompareTag("Finish"))
+        if (collision.collider.CompareTag("Floor"))
         {
             isJump = false;
         }
-        else if (collision.collider.CompareTag("EditorOnly"))
+        else if (collision.collider.CompareTag("Walls"))
         {
             ChangeDirection();
         }
@@ -76,24 +120,8 @@ public class EnemyMovement : MonoBehaviour
 
     private void ChangeDirection()
     {
-        enemySpriteRender.flipX = !enemySpriteRender.flipX;
-        isMovingLeft = !isMovingLeft;
-        speedMovemen *= -changeDirection;
-    }
+        changeDirection *= -1;
 
-    private void Jump()
-    {
-        isJump = true;
-
-        AudioManager.instance.PlaySFX(jumpAudioClip);
-
-        if (!isMovingLeft)
-        {
-            enemyRigidbody2D.AddForce(new Vector2(5, 5), ForceMode2D.Impulse);
-        }
-        else
-        {
-            enemyRigidbody2D.AddForce(new Vector2(5 * -changeDirection, 5), ForceMode2D.Impulse);
-        }
+        enemySpriteRender.flipX = true;
     }
 }
